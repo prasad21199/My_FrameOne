c66a46d14ba34310e5063da3493acfabb7fabe95
prasadk <prasadk@LAPTOP-OR2KO8HI>
2025-02-27 09:50:31 +0530

    prasad first

---
+++ Jenk/src/test/java/pack1/ClassA.java
@@ -0,0 +1,16 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+	@Test
+	public void test1()
+	{
+		System.out.println("test 1 method");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("test 2 method");
+	}
+}
---
+++ Jenk/src/test/java/pack2/ClassB.java
@@ -0,0 +1,16 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+	@Test
+	public void testb()
+	{
+		System.out.println("testb  method from class b");
+	}
+	@Test
+	public void testc()
+	{
+		System.out.println("testc  method from class b");
+	}
+}
